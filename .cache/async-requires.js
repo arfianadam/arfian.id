d0a692e5c38f4cb98b580510bdae2198ac41fedb
Arfian Adam <arfianadam@yahoo.com>
2020-09-09 12:48:27 +0700

    added some setup

--- .cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-exports.components = {
-  "component---node-modules-gatsby-theme-blog-core-src-templates-post-query-js": () => import("./../../../node_modules/gatsby-theme-blog-core/src/templates/post-query.js" /* webpackChunkName: "component---node-modules-gatsby-theme-blog-core-src-templates-post-query-js" */),
-  "component---node-modules-gatsby-theme-blog-core-src-templates-posts-query-js": () => import("./../../../node_modules/gatsby-theme-blog-core/src/templates/posts-query.js" /* webpackChunkName: "component---node-modules-gatsby-theme-blog-core-src-templates-posts-query-js" */)
-}
-
